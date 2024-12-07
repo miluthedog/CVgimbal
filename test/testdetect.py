064eb719b029e635d6e7760f4496b2505dacd91d
Pha <anhphamhoang04@gmail.com>
2024-12-08 00:39:49 +0700

    test

--- test/testdetect.py
+++ test/testdetect.py
@@ -1,8 +1,7 @@
-import cv2
-import time
+import cv2 as cv
 
-trainedData = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-cam = cv2.VideoCapture(0)
+trainedData = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+cam = cv.VideoCapture(0)
 
 while True:
     running, info = cam.read()
@@ -10,19 +9,19 @@ while True:
     if not running:
         break
 
-    gray = cv2.cvtColor(info, cv2.COLOR_BGR2GRAY)
+    gray = cv.cvtColor(info, cv.COLOR_BGR2GRAY)
     faces = trainedData.detectMultiScale(gray, scaleFactor = 1.1, minNeighbors = 5)
 
     if len(faces) > 0:
         (x, y, w, h) = faces[0]
-        cv2.rectangle(info, (x, y), (x+w, y+h), (255, 255, 0), 2)
+        cv.rectangle(info, (x, y), (x+w, y+h), (255, 255, 0), 2)
 
-    cv2.imshow('Tracking', info)
+    cv.imshow('Tracking', info)
 
-    pressEsc = cv2.waitKey(1) & 0xff
+    pressEsc = cv.waitKey(1) & 0xff
     if pressEsc == 27:
         break
 
 # clean up
 cam.release()
-cv2.destroyAllWindows()
+cv.destroyAllWindows()
--- test/testtrack.py
+++ test/testtrack.py
@@ -1,13 +1,13 @@
-import cv2
+import cv2 as cv
 import serial
 import time
 
 # set up cv
-trainedData = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-cam = cv2.VideoCapture(0)
+trainedData = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+cam = cv.VideoCapture(0)
 
 # set up arduino
-arduino = serial.Serial('COM5', 9600)
+arduino = serial.Serial('COM5', 9600) # adjust this
 time.sleep(2)
 
 while True:
@@ -16,13 +16,13 @@ while True:
     if not running:
         break
 
-    gray = cv2.cvtColor(info, cv2.COLOR_BGR2GRAY)
+    gray = cv.cvtColor(info, cv.COLOR_BGR2GRAY)
     faces = trainedData.detectMultiScale(gray, scaleFactor = 1.1, minNeighbors = 5)
 
     if len(faces) > 0:
         # draw rectangle: (x, y) are coord of top left pixel, w h are width height - BRG color
         (x, y, w, h) = faces[0]
-        cv2.rectangle(info, (x, y), (x+w, y+h), (255, 255, 0), 2)
+        cv.rectangle(info, (x, y), (x+w, y+h), (255, 255, 0), 2)
         
         # send signal to arduino
         width = info.shape[1]
@@ -30,13 +30,13 @@ while True:
         normalizedX = int((center / width) * 180)
         arduino.write((str(normalizedX) + '\n').encode())
 
-    cv2.imshow('Tracking', info)
+    cv.imshow('Tracking', info)
 
-    pressEsc = cv2.waitKey(1) & 0xff
+    pressEsc = cv.waitKey(1) & 0xff
     if pressEsc == 27:
         break
 
 # clean up
 cam.release()
-cv2.destroyAllWindows()
+cv.destroyAllWindows()
 arduino.close()
